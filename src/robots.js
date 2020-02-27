0b157d10955b134f591bb63d3678c862c8d1c2e1
gabiristea <ristea.gabi@gmail.com>
2020-02-27 15:10:13 +0200

    project finished

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Gabriel Ristea',
+    username: 'Elos',
+    email: 'Elos@gmail.com'
+  },
+  {
+    id: 2,
+    name: 'Alexandra Ristea',
+    username: 'Alexa',
+    email: 'Alexa@gmail.net'
+  },
+  {
+    id: 3,
+    name: 'Ciprian Ifrim',
+    username: 'Despinul',
+    email: 'Despinul@yesenia.net'
+  },
+  {
+    id: 4,
+    name: 'Alina Ionita',
+    username: 'Blu',
+    email: 'Blu_cat@kory.org'
+  },
+  {
+    id: 5,
+    name: 'Raicovici Mario',
+    username: 'Mario',
+    email: 'Mario@annie.ca'
+  },
+  {
+    id: 6,
+    name: 'Alexandra Raicovici',
+    username: 'Alexandra',
+    email: 'Alexandra@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Mircea Popa',
+    username: 'Mircea.Popa',
+    email: 'Mircea.Popa@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Udeanu Camelia',
+    username: 'Cameliu',
+    email: 'Cameliu@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Nedea Mihai',
+    username: 'Tigaita',
+    email: 'Tigaita_Boss@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Istinie Codrul',
+    username: 'Omul_Reclama',
+    email: 'Omul_Reclamag@karina.biz'
+  }
+];
